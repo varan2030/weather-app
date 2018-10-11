ec285de50e2cc7c082aae7a4feb1bca9d7d275e9
varan2030 <varan2030@>
2018-10-10 21:40:38 -0400

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CurrentWeatherComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/current-weather/current-weather.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-current-weather',
+  template: `
+  <p>
+  current-weather works
+  </p>`,
+  styles: ['']
+})
+export class CurrentWeatherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
